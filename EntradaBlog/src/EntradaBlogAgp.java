ffa1a7084c839f0db4580968c2f5d1bd51683d0d
Aroa <aroagpclase@gmail.com>
2022-02-21 23:26:24 +0100

    EntradaBlogAgp

---
+++ EntradaBlog/src/EntradaBlogAgp.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ * @author Aroa Guirado Picazo
+ *20 feb 2022
+ */
+
+public class EntradaBlogAgp {
+	
+	
+	/**Clase creada el 20/02/2022
+	 * Por Aroa Guirado Picazo
+	 */
+	
+		
+		public static char separador=':';
+		private int id;
+		private String texto;
+		private String autor;
+		
+		/**Excepción que impedia que el programa se ejecutase correctamente
+		 *  debido a la introducción de un numero negativo en el main
+		 */
+		public EntradaBlogAgp(int id,String autor,String texto)throws IllegalArgumentException{
+			if(id<=0) throw new IllegalArgumentException("El id no puede ser negativo");
+			this.id=id;
+			this.autor=autor;
+			this.texto=texto;
+		}
+		@Override
+		public String toString(){
+			String cad="";
+			cad+="\nENTRADA DE"+separador+autor;
+			cad+="\n "+texto;
+			return cad;
+		}
+		
+		
+		public int getId(){
+			return this.id;
+		}
+		
+		
+		public String getTexto(){
+			return this.texto;
+		}
+		
+		
+		public String getAutor(){
+			return this.autor.toUpperCase();
+		}
+		
+		
+		public String devuelveAutor(){
+			return this.autor;
+		}
+		
+		
+		public static void main(String[] args) {
+	        /**Debemos cambiar el número de entrada, 
+	         * dado que tenemos una excepción que nos impide que el número sea negativo*/
+			
+			EntradaBlogAgp e1=new EntradaBlogAgp (3,"ana","Últimas noticias, está disponible BixBy 2.0");
+			
+			/**Creo otra entrada en el brlog para comprobar que funciona correctamente*/
+			  
+			EntradaBlogAgp e2=new EntradaBlogAgp (5,"María", "Lost Ark está disponible en Steam.");
+			
+			System.out.println(e1);
+			System.out.print(e2);
+}
+}
